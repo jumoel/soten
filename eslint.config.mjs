fcd22dd1185f84f15c007c71a7e3feb6dc9d7eb2
Julian Møller Ellehauge <git@jumoel.com>
2025-03-20 10:29:32 +0100

    refactor: switch to vite and plain react instead of nextjs (#29)
    Next was a bit too inflexible after all.
    
    bonus: add needed `isomorphic-git` deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  includeIgnoreFile(gitignorePath),
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
 
-export default eslintConfig;
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"], // For React 17+
+
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
