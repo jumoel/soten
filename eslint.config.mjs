5033f0ddd14b0eea27176a56207050b3a8e5d8e0
Julian Møller Ellehauge <git@jumoel.com>
2025-03-20 10:24:41 +0100

    fix linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,34 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
 import tseslint from "typescript-eslint";
-import pluginReact, { rules } from "eslint-plugin-react";
+import pluginReact from "eslint-plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  includeIgnoreFile(gitignorePath),
+
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
 
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"], // For React 17+
 
   {
-    rules: {
-      "react/react-in-jsx-scope": "off",
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
 ];
