690cc36fe220d43d24156cbdbd4aa93ecf8e37bf
Julian Møller Ellehauge <git@jumoel.com>
2025-04-24 13:13:17 +0200

    different wipe

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,21 +1,12 @@
-import { pfs, FILE_SYSTEM_NAME } from "./fs";
+import { wipeFs } from "./fs";
 import * as git from "./git";
 
 export async function init(repoName: string, user: { username: string; token: string }) {
-  await indexedDB.deleteDatabase(FILE_SYSTEM_NAME);
-
-  const repoExists = await pfs
-    .readdir(git.REPO_DIR)
-    .then(() => true)
-    .catch(() => false);
+  await wipeFs();
 
   console.log("setting user");
   await git.setUser();
 
-  if (!repoExists) {
-    console.log("cloning");
-    await git.clone(`https://github.com/${repoName}`, user);
-  }
-
-  console.log("pulling");
+  console.log("cloning");
+  await git.clone(`https://github.com/${repoName}`, user);
 }
